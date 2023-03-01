768b0f646750fd3f1f30ff97488e4542a6572fa0
yusaku <0227493@mchcgr.com>
2023-03-01 12:36:19 +0900

    first commit

---
+++ GitTestRemote/GitTestRemote.h
@@ -0,0 +1,32 @@
+﻿
+// GitTestRemote.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CGitTestRemoteApp:
+// このクラスの実装については、GitTestRemote.cpp を参照してください
+//
+
+class CGitTestRemoteApp : public CWinApp
+{
+public:
+	CGitTestRemoteApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGitTestRemoteApp theApp;
